0be154993019475d854ab5cf6c8cda286b5312c5
Aleksan-dres <galeks382@gmail.com>
2026-01-02 18:24:03 +0300

    Update models.py
    Добавил модели

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,3 +1,34 @@
 from django.db import models  # noqa F401
 
-# your models here
+
+class Pokemon(models.Model): 
+    title = models.CharField(verbose_name='имя',max_length=200) 
+    title_en = models.CharField(verbose_name='имя на английском', max_length=200,blank=True) 
+    title_jp = models.CharField(verbose_name='имя на японском', max_length=200,blank=True)
+    description = models.TextField(verbose_name='описание покемона ', blank=True)
+    photo = models.ImageField(verbose_name="картинка", null=True, blank=True) 
+    previous_evolution = models.ForeignKey(
+        'self', 
+        verbose_name='из кого эволюционирует', 
+        null=True, 
+        blank=True, 
+        related_name='next_evolutions', 
+        on_delete=models.SET_NULL
+    )
+
+
+    def __str__(self): 
+        return f'{self.title}'  
+
+
+class PokemonEntity(models.Model): 
+    pokemon_model = models.ForeignKey(Pokemon,verbose_name='покемон', on_delete=models.CASCADE,related_name='entities') 
+    lat = models.FloatField(verbose_name='широта') 
+    lon = models.FloatField(verbose_name='долгота') 
+    appeared_at = models.DateTimeField(verbose_name='появление', null=True, blank=True) 
+    disappeared_at = models.DateTimeField(verbose_name='исчезновение', null=True, blank=True) 
+    level = models.IntegerField(verbose_name='уровень', null=True, blank=True)
+    health = models.IntegerField(verbose_name='здоровье', null=True, blank=True)
+    attack = models.IntegerField(verbose_name='сила атаки', null=True, blank=True)
+    defense = models.IntegerField(verbose_name='защита', null=True, blank=True)
+    stamina = models.IntegerField(verbose_name='выносливость', null=True, blank=True) 
